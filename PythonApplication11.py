7cce9d75913980ef692cc7cdb58351ae16777a2d
ZKsenia <91132114+ZKsenia@users.noreply.github.com>
2021-10-26 15:07:04 +0300

    Update PythonApplication11.py

--- PythonApplication11.py
+++ PythonApplication11.py
@@ -1,30 +1,37 @@
 #11.1 
+print('1)Введите значения двух переменных А и В')
 A = int(input())
 B = int(input())
 if (A==B):
     A = 0
     B = 0
-    print(A,B)
+    print('ответ =',A,B, sep = ' ')
 else:
     A = max(A,B)
     B = max(A,B)
-    print(A,B)
+    print('ответ =',A,B, sep = ' ')
 #11.2
+print('2)Введите три числа a,b,c')
 a = int(input())
 b = int(input())
 c = int(input())
-print(max(a,b,c) + (a+b+c-min(a,b,c)-max(a,b,c)))
+print('ответ =',max(a,b,c) + (a+b+c-min(a,b,c)-max(a,b,c)),sep = ' ')
 #11.3
-a = int(input())
-b = int(input())
-c = int(input())
-ab = abs(a-b)
-ac = abs(a-c)
+print('3)Введите координаты точек a,b,c')
+xa = int(input())
+ya = int(input())
+xb = int(input())
+yb = int(input())
+xc = int(input())
+yc = int(input())
+ab = ((xb-xa)**2 + (yb-ya)**2)**0.5
+ac = ((xc-xa)**2 + (yc-ya)**2)**0.5
 if ab<ac:
     print('B, расстояние равно', ab)
 elif ac<ab:
     print('C, расстояние равно', ac)
 #11.4
+print('4)Введите координаты точки')
 x = int(input())
 y = int(input())
 if ((x!=0) and (y!=0)):
@@ -39,18 +46,21 @@ if ((x!=0) and (y!=0)):
 else:
     print('Введённые координаты не удовлетворяют условию')
 #11.5
+print('5)Введите число')
 n = int(input())
-if ((n>0) and (n%2==0)):
-    print('Положительное чётное число')
-if ((n>0) and (n%2!=0)):
-    print('Положительное нечётное число')
-if ((n<0) and (n%2==0)):
-    print('Отрицательное чётное число')
-if ((n<0) and (n%2!=0)):
-    print('Отрицательное нечётное число')
-else:
+if (n==0):
     print('Нулевое число')
+else:
+    if ((n>0) and (n%2==0)):
+        print('Положительное чётное число')
+    elif ((n>0) and (n%2!=0)):
+        print('Положительное нечётное число')
+    elif ((n<0) and (n%2==0)):
+        print('Отрицательное чётное число')
+    elif ((n<0) and (n%2!=0)):
+        print('Отрицательное нечётное число')
 #11.6 
+print('6)Введите число')
 n = int(input())
 if n in range(1,999+1):
     if ((n>=1) and (n<=9) and (n%2==0)):
