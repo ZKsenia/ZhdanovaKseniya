f9556895bd4d7e7ba0d99d328521a6c4beac4f28
ZKsenia <91132114+ZKsenia@users.noreply.github.com>
2021-10-31 15:57:27 +0300

    Update PythonApplication15.py

--- PythonApplication15.py
+++ PythonApplication15.py
@@ -84,3 +84,21 @@ if ((x3!=0) and (y3!=0)):
 else:
     print('Введённые координаты не удовлетворяют условию')
  
+#15.5
+print('Введите число')
+def Fact2(n):
+    x = 1
+    if (n%2 == 0):
+        y = 2
+        while y <= n:
+            x = x * y
+            y = y + 2 
+        return x
+    else:
+        y = 3
+        while y <= n:
+            x = x * y
+            y = y + 2
+        return x
+n=int(input())
+print('ответ =',Fact2(n),sep=' ')
