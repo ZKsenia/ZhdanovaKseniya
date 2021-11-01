d2fb11ab026813984a0f22e6b36c5dce95a645e2
ZKsenia <91132114+ZKsenia@users.noreply.github.com>
2021-11-01 13:17:49 +0300

    Update PythonApplication16.py

--- PythonApplication16.py
+++ PythonApplication16.py
@@ -8,3 +8,19 @@ if (n>0):
     print('ответ =',a, sep = ' ')
 else:
     print('Ввеите значение, удовлетворяющее условию')
+    
+#16.2
+print('Введите N, N>1')
+n = int(input())
+print('Введите A')
+a = int(input())
+print('Введите D')
+d = int(input())
+m=[]
+if (n>1):
+    for i in range(1,n+1):
+        m.append(a*d)
+        a = a*d
+    print('ответ =',m, sep = ' ')
+else:
+    print('Ввеите значение, удовлетворяющее условию')
